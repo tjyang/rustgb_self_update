dd96f65aa5186907949522e75aabec20b471c929
T.J. Yang <tjyang2001@gmail.com>
2020-06-13 11:53:28 -0500

    adding src code

---
+++ src/gh-selfupdate.rs
@@ -0,0 +1,44 @@
+/*!
+Example updating an executable to the latest version released via GitHub
+*/
+
+// For the `cargo_crate_version!` macro
+#[macro_use]
+extern crate self_update;
+
+fn run() -> Result<(), Box<dyn ::std::error::Error>> {
+    let releases = self_update::backends::github::ReleaseList::configure()
+        .repo_owner("tjyang")
+        .repo_name("rustgb_self_update")
+        .build()?
+        .fetch()?;
+    println!("found releases:");
+    println!("{:#?}\n", releases);
+
+    let status = self_update::backends::github::Update::configure()
+        .repo_owner("tjyang")
+        .repo_name("learnrut/rustgb_self_update")
+        .bin_name("ghupdate01") 
+        .show_download_progress(true)
+        //.target_version_tag("v9.9.9")
+        //.show_output(false)
+        //.no_confirm(true)
+        //
+        // For private repos, you will need to provide a GitHub auth token
+        // **Make sure not to bake the token into your app**; it is recommended
+        // you obtain it via another mechanism, such as environment variables
+        // or prompting the user for input
+        //.auth_token(env!("DOWNLOAD_AUTH_TOKEN"))
+        .current_version(cargo_crate_version!())
+        .build()?
+        .update()?;
+    println!("Update status: `{}`!", status.version());
+    Ok(())
+}
+
+pub fn main() {
+    if let Err(e) = run() {
+        println!("[ERROR] {}", e);
+        ::std::process::exit(1);
+    }
+}
